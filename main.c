ff646c701f9439a4c2e19ded181f655872e6f354
ryanmusic10 <ryansengupt@gmail.com>
2026-06-29 20:10:06 +0530

    Day 1:Variables and Data Types

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdbool.h>
+// This is a goated program
+
+/*  
+Woah
+This 
+is 
+Awesome
+
+*/
+
+int main() {
+
+int age = 18;
+int year = 2026;
+int quantity = 100;
+float gpa = 10;
+float price = 200.5;
+float temperature = -10.5;
+double pi = 3.14159265358979323846;
+double e = 2.71828182845904523536;
+char grade = 'A';
+char symbol = '?';
+char currency = '$';
+char name[] = "Ryan Sengupta";
+char food[] = "Biryani";
+char email[] = "fakeemail123@gmail.com";
+bool isOnline = true;
+
+printf("My age is %d\n", age);
+printf("The year is %d\n", year);
+printf("That is a %d rs toy\n", quantity);
+printf("My gpa is %f\n", gpa);
+printf("The price of the toy is increased to %f\n", price);
+printf("The temperature is %f degrees \n", temperature);
+printf("The value of pi is .%.16lf\n",pi);
+printf("The value of e is .%.16lf\n",e);
+printf("Your grade in %c\n",grade);
+printf("Who are you mate%c\n", symbol);
+printf("Pay up brother is %c\n", currency);
+printf("My name is %s\n", name);
+printf("I am currently eating %s\n", food);
+printf("You are using email named %s\n", email);
+printf("Is he Online? %d\n", isOnline);
+
+return 0;
+
+}
