d6c21ab4ca425e218b165a54b2b83cda9756978f
ryanmusic10 <ryansengupt@gmail.com>
2026-06-29 21:07:13 +0530

    Arithmetic operators in C

---
+++ arithmetic.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    
+    // arithmetic operations = + - * / % ++ --
+
+    int a = 10;
+    int b = 40;
+    int c=0;
+
+    //c= a + b;
+    //c= a-b;
+    //c=a*b;
+    //c=a/b;
+    //c= a%b;
+
+    //augmented assignment operators = += -= *= /=
+
+    int x = 5;
+    //x++; // increment operator
+    int y= 6;
+    //y--; // decrement operator
+    //x+= 3; // x = x + 3
+    //y-= 1; // y = y - 1
+    //x*= 2; // x = x * 2
+    //y/= 3; // y = y / 3
+
+
+    printf("The remainder is %d\n", c);
+    printf("The value of x is %d\n", x);
+    printf("The value of y is %d\n", y);
+    return 0;
+}
